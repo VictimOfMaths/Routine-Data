cdf8e8a649ce50b637c2c5b20405964a5b8aae0e
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-04-15 19:40:20 +0100

    Added Scotland to the regional plot

--- ONSWeeklyDeaths.R
+++ ONSWeeklyDeaths.R
@@ -267,3 +267,80 @@ ggplot()+
   geom_text(data=ann_text2, aes(x=weekno, y=deaths), label=c("2020", "Max", "Min"), size=3, 
             colour=c("Red", "deepskyblue4", "deepskyblue4"))
 dev.off()  
+
+#################################
+#Bring in Scottish data from NRS#
+#################################
+
+temp <- tempfile()
+source <- "https://www.nrscotland.gov.uk/files//statistics/weekly-monthly-births-deaths-data/2020/mar/weekly-march-20.xlsx"
+temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+data10 <- read_excel(temp, sheet="2010", range="A6:D57", col_names=FALSE)
+data11 <- read_excel(temp, sheet="2011", range="A6:D58", col_names=FALSE)
+data12 <- read_excel(temp, sheet="2012", range="A6:D57", col_names=FALSE)
+data13 <- read_excel(temp, sheet="2013", range="A6:D57", col_names=FALSE)
+data14 <- read_excel(temp, sheet="2014", range="A6:D57", col_names=FALSE)
+data15 <- read_excel(temp, sheet="2015 ", range="A6:D58", col_names=FALSE)
+data16 <- read_excel(temp, sheet="2016", range="E6:G57", col_names=FALSE)
+data17 <- read_excel(temp, sheet="2017", range="E6:G57", col_names=FALSE)
+data18 <- read_excel(temp, sheet="2018", range="E6:G57", col_names=FALSE)
+data19 <- read_excel(temp, sheet="2019", range="E6:G57", col_names=FALSE)
+data20 <- read_excel(temp, sheet="2020", range="E6:G19", col_names=FALSE)
+
+data10$year <- "2010"
+data11$year <- "2011"
+data12$year <- "2012"
+data13$year <- "2013"
+data14$year <- "2014"
+data15$year <- "2015"
+data16$year <- "2016"
+data17$year <- "2017"
+data18$year <- "2018"
+data19$year <- "2019"
+data20$year <- "2020"
+
+#Stick together 2004-15 which share the same structure
+data1015 <- bind_rows(data10, data11, data12, data13, data14, data15)
+colnames(data1015) <- c("weekno", "date", "births", "deaths", "year")
+
+#Then 2016-20 data
+data1620 <- bind_rows(data16, data17, data18, data19, data20)
+colnames(data1620) <- c("weekno", "date", "deaths", "year")
+
+data <- bind_rows(data1015, data1620)
+
+#Extract max/min values
+#split off 2020 data
+data_new <- subset(data, year=="2020")
+data_old <- subset(data, year!="2020")
+
+data_old <- data_old %>%
+  group_by(weekno) %>%
+  summarise(max=max(deaths), min=min(deaths))
+
+data_old$reg <- "Scotland"
+data_new$reg <- "Scotland"
+data_new$year <- as.numeric(data_new$year)
+
+data_rold <- bind_rows(data_rold, data_old)
+data_rnew <- bind_rows(data_rnew, data_new)
+
+ann_text3 <- data.frame(weekno=c(17.2, 32, 32), deaths=c(1720, 1200,700), lab=c("2020", "Max", "Min"), 
+                        reg=rep("East", times=3))
+
+tiff("Outputs/ONSNRSWeeklyDeaths_reg.tiff", units="in", width=12, height=8, res=300)
+ggplot()+
+  geom_ribbon(data=data_rold, aes(x=weekno, ymin=min, ymax=max), fill="Skyblue2")+
+  geom_line(data=data_rnew, aes(x=weekno, y=deaths), colour="Red")+
+  theme_classic()+
+  facet_wrap(~reg)+
+  scale_x_continuous(name="Week number")+
+  scale_y_continuous(name="Deaths registered")+
+  theme(strip.background=element_blank(), strip.text=element_text(face="bold"))+
+  labs(title="Deaths from all causes have risen sharply across Great Britain",
+       subtitle="Weekly deaths in 2020 compared to the range in 2010-19",
+       caption="Data from ONS & NRS | Plot by @VictimOfMaths")+
+  geom_text(data=ann_text3, aes(x=weekno, y=deaths), label=c("2020", "Max", "Min"), size=3, 
+            colour=c("Red", "deepskyblue4", "deepskyblue4"))
+dev.off()  
+
